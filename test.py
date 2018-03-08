5e3449e9d921b4628a508df0dc818021476897d9
duoduo <duoduo@happy.com>
2018-03-09 00:48:43 +0800

    delete unused code in test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-
 def ImageToMatrix(filename):
     im = Image.open(filename) 
     #im.show()
@@ -22,6 +21,5 @@ with tf.Session() as sess:
     y_conv = graph.get_tensor_by_name("Softmax:0")
     keep_prob = graph.get_tensor_by_name("Placeholder_2:0")
     saver.restore(sess, "./weight/model.ckpt")
-    x = ImageToMatrix("4.png")
     output =  sess.run(y_conv, {x_input: ImageToMatrix("4.png"), keep_prob: 1.0})
     print "The num is", tf.argmax(output, 1).eval()
